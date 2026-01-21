a6ff872e1cb0e64069d289dd1273ed6586278184
mdsamiulislam54 <samiulm5332@gmail.com>
2026-01-21 12:02:27 +0600

    change: remove large file .mp4

--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -1,12 +1,19 @@
 
 import Banner from "@/components/page/Banner/Banner";
-import { authClient } from "@/lib/auth-client";
-
-
-
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const session = await authClient.getSession();
+  const cookieStore = await cookies();
+  
+  const session = await fetch(`${process.env.BACKEND_URL}/api/auth/get-session`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: cookieStore.toString()
+    },
+    cache: "no-store",
+    credentials: "include",
+  }).then((res) => res.json());
   console.log("Home page session", session)
   return (
     <div>
--- src/hooks/useSession/useSession.ts
+++ src/hooks/useSession/useSession.ts
@@ -3,7 +3,6 @@ import { authClient } from "@/lib/auth-client"
 
 const getUser = ()=>{
     const session =  authClient.useSession();
-    console.log(session)
     return session.data?.user;
 }
 
